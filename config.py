8b64d35d8e87746fa552307ecb504e158a3f841f
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-04 08:27:04 +0300

    This commits updates till sending emails

--- config.py
+++ config.py
@@ -1,39 +1,37 @@
 import os
 
 class Config:
-    '''
-    General configuration parent class
-    '''
+
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY =os.environ.get('MOVIE_API_KEY')
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:xoxo@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Watchlist'
+    SENDER_EMAIL = 'richard.omondi@student.moringaschool.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
+
 }
-
-
-
-
