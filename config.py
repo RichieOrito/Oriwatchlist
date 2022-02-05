7cf2590dc6817838bcbedc0d72569c187c3f75e8
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-05 16:18:15 +0300

    db url

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class Config:
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
 class TestConfig(Config):
